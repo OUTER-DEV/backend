9d01b7c804e91c553137fa7ef22804f130740845
Jonatana RANDRIAMPARANY Andrianina <Jonas4884>
2024-01-26 23:48:24 +0300

    chore: initiate babyearn project

---
+++ src/main/java/com/hackathon/babyearn/BabyEarnApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.babyearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyEarnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabyEarnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hackathon/babyearn/BabyEarnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.babyearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabyEarnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
